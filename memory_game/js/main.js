0f57c22d7281c9392fbb05436836e9a5cdedc63a
Ana Silva <silva@Anas-Maqui.local>
2019-09-20 15:59:47 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,24 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log(cardsInPlay)
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	console.log(cardsInPlay)
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+
+flipCard(0);
+flipCard(2);
